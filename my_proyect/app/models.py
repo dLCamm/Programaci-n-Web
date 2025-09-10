4b1a5be180f71b5a9cf14eaf41675fe57eb4a078
dLCamm <camilodeleon229@gmail.com>
2025-09-09 21:17:57 -0600

    feat(models): new database model "job"

--- my_proyect/app/models.py
+++ my_proyect/app/models.py
@@ -9,4 +9,9 @@ class Persona(models.Model):
     def __str__(self):
         return self.name
 
-    
+class Job(models.Model):
+    namejob = models.CharField(max_length=(100))
+    person = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre    
